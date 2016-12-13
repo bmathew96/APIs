e675cd9cb4beb03c14ef9d6ee63d3606caad47e8
Bejoy <bmathew@redventures.com>
2016-12-12 21:19:04 -0500

    added env variables and models

---
+++ go-project/env.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+/*
+	These environment variables must be set in order for getEnvConfig() to work
+	export MYAPI_DBUSERNAME=
+	export MYAPI_DBPASSWORD=
+	export MYAPI_DBHOSTNAME=
+	export MYAPI_DBPORT=3306
+	export MYAPI_DEBUG=true
+	export MYAPI_APIKEYPUBLIC=""
+	export MYAPI_APIKEYPRIVATE=""
+*/
+
+// EnvConfig contains the Enviornment Variables we are using
+type EnvConfig struct {
+	DBUsername    string
+	DBPassword    string
+	DBHostname    string
+	APIKeyPublic  string `envconfig:"APIKeyPublic"`
+	APIKeyPrivate string `envconfig:"APIKeyPrivate"`
+	DBPort        int
+	Debug         bool
+}
+
+func getEnvConfig() (config EnvConfig, err error) {
+
+	// parse out environment variables
+	err = envconfig.Process("myapi", &config)
+
+	return
+}
+
+func getKeyHash() (hash string, timeStamp string) {
+	timeStamp = time.Now().String()
+
+	//creates md5 hash
+	h := md5.New()
+	io.WriteString(h, timeStamp)
+	io.WriteString(h, config.APIKeyPrivate)
+	io.WriteString(h, config.APIKeyPublic)
+	hash = fmt.Sprintf("%x", h.Sum(nil))
+
+	return
+}
